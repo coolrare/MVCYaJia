f8cf7ad5bde90cb71ae391510104678590eee5fb
Will保哥 <doggy.huang@gmail.com>
2017-07-03 15:07:38 +0800

    08 練習透過擴充 Model Class 自訂額外的驗證屬性

---
+++ MVCYaJia/Models/Product.Partial.cs
@@ -0,0 +1,32 @@
+namespace MVCYaJia.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "必填欄位: {0}")]
+        [DisplayName("商品名稱")]
+        public string ProductName { get; set; }
+        [Required]
+        [DisplayName("商品價格")]
+        [DisplayFormat(DataFormatString = "{0:0}")]
+        public Nullable<decimal> Price { get; set; }
+        [Required]
+        [DisplayName("是否上架")]
+        public Nullable<bool> Active { get; set; }
+        [Required]
+        [DisplayName("庫存量")]
+        public Nullable<decimal> Stock { get; set; }
+
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
