fd5c9f28feaec5aac3eaab7ddffa03abe9467a9f
Will保哥 <doggy.huang@gmail.com>
2017-07-03 14:24:14 +0800

    06 練習透過 LoginViewModel 實作登入頁面

---
+++ MVCYaJia/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCYaJia.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DisplayName("帳號")]
+        public string Username { get; set; }
+        [Required]
+        [DisplayName("密碼")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
