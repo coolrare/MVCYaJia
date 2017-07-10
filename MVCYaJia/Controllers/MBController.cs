6e9117438f91b9424a553c5a3ef9512aaf88b97b
Will保哥 <doggy.huang@gmail.com>
2017-07-10 14:49:57 +0800

    19 練習簡單模型繫結與複雜模型繫結

---
+++ MVCYaJia/Controllers/MBController.cs
@@ -0,0 +1,35 @@
+﻿using MVCYaJia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCYaJia.Controllers
+{
+    public class MBController : BaseController
+    {
+        public ActionResult Index(int id)
+        {
+            ViewData["T1"] = id;
+            ViewBag.T1 = id;
+
+            ViewData.Model = repo.Find(1);
+
+            return View();
+        }
+
+        public ActionResult ProductEdit(int id)
+        {
+            return View(repo.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult ProductEdit(int id, Product data)
+        {
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
