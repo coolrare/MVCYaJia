17c994fbca49a868dd112c183e7c576b04862c7c
Will保哥 <doggy.huang@gmail.com>
2017-07-17 17:10:04 +0800

    30 練習 @Html.DisplayFor() 與 @Html.EditorFor() 自訂模型樣板

--- MVCYaJia/Controllers/OrderLinesController.cs
+++ MVCYaJia/Controllers/OrderLinesController.cs
@@ -17,7 +17,7 @@ namespace MVCYaJia.Controllers
         [ChildActionOnly]
         public ActionResult Index(int id)
         {
-            var orderLine = db.OrderLine.Where(p => p.ProductId == id).Include(o => o.Order).Include(o => o.Product);
+            var orderLine = db.OrderLine.Where(p => p.ProductId == id).Include(o => o.Order).Include(o => o.Product).Take(10);
             return View(orderLine.ToList());
         }
         protected override void Dispose(bool disposing)
