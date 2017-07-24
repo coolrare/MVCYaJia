94a19a6cf47b53f285c8192d366d9b4dfd5e3029
Will保哥 <doggy.huang@gmail.com>
2017-07-24 11:53:10 +0800

    35 建立 ASP.NET Web API 的 Models 與 ApiController

---
+++ WebApiYaJia/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiYaJia.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApiYaJia/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WebApiYaJia.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
